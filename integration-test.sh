20e545cc965d0a0a69941cd00f00cd32deb14b7d
NavnathChaudhari <98884491+NavnathChaudhari@users.noreply.github.com>
2022-10-13 17:42:04 +0530

    Update integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -1,27 +1,16 @@
-#!/bin/bash
-
 #integration-test.sh
 
 sleep 5s
 
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n default get svc devsecops-svc -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
-echo $applicationURL:$PORT/$applicationURI
+echo $applicationURL:$PORT
 
 if [[ ! -z "$PORT" ]];
 then
 
-    response=$(curl -s $applicationURL:$PORT$applicationURI)
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
-
-    if [[ "$response" == 100 ]];
-        then
-            echo "Increment Test Passed"
-        else
-            echo "Increment Test Failed"
-            exit 1;
-    fi;
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT)
 
     if [[ "$http_code" == 200 ]];
         then
