89079e842cf203ee9eb88c6f1c2156821fcba084
NavnathChaudhari <98884491+NavnathChaudhari@users.noreply.github.com>
2022-11-10 16:04:52 +0530

    Update cis-master.sh

--- cis-master.sh
+++ cis-master.sh
@@ -5,7 +5,7 @@ total_fail=$(kube-bench master  --version 1.15 --check 1.2.7,1.2.8,1.2.9,1.3.2 -
 
 if [[ "$total_fail" -ne 0 ]];
         then
-                echo "CIS Benchmark Failed MASTER while testing for 1.2.7, 1.2.8, 1.2.9"
+                echo "CIS Benchmark Failed MASTER while testing for 1.2.7, 1.2.8, 1.2.9, 1.3.2"
                 exit 1;
         else
                 echo "CIS Benchmark Passed for MASTER - 1.2.7, 1.2.8, 1.2.9"
